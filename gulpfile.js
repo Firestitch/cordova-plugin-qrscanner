bce8c0051d7b841e654e2b9ce09529d1b4fc5614
Raymond Gigliotti <ray@firestitch.com>
2023-05-16 16:55:26 -0400

    upgraded to gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ const fs= require('fs');
 
 const remap = fs.readFileSync('src/common/src/cordova-remap.js', 'utf-8');
 
-function webpack(config, callback){
+function webpack(config, callback) {
   const exec = require('child_process').exec;
   exec(__dirname + '/node_modules/.bin/webpack --config ' + config, (error, stdout, stderr) => {
     console.log(stdout);
@@ -15,32 +15,32 @@ function webpack(config, callback){
   });
 }
 
-gulp.task('prepack', function(cb){
+gulp.task('prepack', function(cb) {
   webpack('webpack.prepack.config.js', cb);
 });
 
-gulp.task('webpack-cordova', ['prepack'], function(cb){
+gulp.task('webpack-cordova',  gulp.series('prepack', function(cb){
   webpack('webpack.cordova.config.js', cb);
-});
+}));
 
-gulp.task('dist', ['prepack'], function(cb){
+gulp.task('dist', gulp.series('prepack', function(cb){
   webpack('webpack.library.config.js', cb);
-});
+}));
 
-gulp.task('remap', ['webpack-cordova'], function () {
+gulp.task('remap', gulp.series('webpack-cordova', function () {
   return gulp.src(['dist/plugin.min.js', 'dist/www.min.js'])
   .pipe(insert.prepend(remap))
   .pipe(gulp.dest('dist'));
-});
+}));
 
-gulp.task('plugin', ['remap'], function () {
+gulp.task('plugin', gulp.series('remap', function () {
   return gulp.src(['dist/plugin.min.js'])
   .pipe(gulp.dest('src/browser'));
-});
+}));
 
-gulp.task('www', ['remap'], function () {
+gulp.task('www', gulp.series('remap', function () {
   return gulp.src(['dist/www.min.js'])
   .pipe(gulp.dest('www'));
-});
+}));
 
-gulp.task('default', ['dist', 'plugin', 'www']);
+gulp.task('default', gulp.series('dist', 'plugin', 'www'));
